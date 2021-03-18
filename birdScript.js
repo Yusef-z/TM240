ded0ded3276ff54482315d69e4fd4884818a65fe
Yusef <yusefzarzar@gmail.com>
2021-03-18 21:09:48 +0300

    fixed issue

--- birdScript.js
+++ birdScript.js
@@ -191,25 +191,14 @@ console.log(hexToDec(wingColor))
 
 
 
-// Get the button that opens the modal
-var btn = fillingButton;
+
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  
-}
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
